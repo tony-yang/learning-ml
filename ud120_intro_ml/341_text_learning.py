52f814b08ad3c48282bab9f5d28b69fd7e60b8e5
Tony Yang <tianyuyang658@yahoo.com>
2018-07-20 22:31:43 -0700

    Implement addition of new feature. Update the text learning to produce full result for future project

--- ud120_intro_ml/341_text_learning.py
+++ ud120_intro_ml/341_text_learning.py
@@ -6,7 +6,6 @@ from nltk.stem.snowball import SnowballStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 def parse_out_text(fh):
-    print('======== parse out text ')
     fh.seek(0)
     all_text = fh.read()
     content = all_text.split('X-FileName:')
@@ -18,7 +17,6 @@ def parse_out_text(fh):
         text_words = text_string.split()
         words = [stemmer.stem(word) for word in text_words]
         words = ' '.join(words)
-        print(words)
     return words
 
 from_sara = open('data/from_sara.txt', 'r')
@@ -27,37 +25,26 @@ from_chris = open('data/from_chris.txt', 'r')
 from_data = []
 word_data = []
 
-temp_counter = 0
 word_to_be_replaced = ['sara', 'shackleton', 'chris', 'germani']
-
 for name, from_person in [('sara', from_sara), ('chris', from_chris)]:
     for path in from_person:
-        temp_counter += 1
-        if temp_counter < 3:
-            path = os.path.join('194_enron', path[:-1])
-            print(path)
-            if os.path.exists(path):
-                email = open(path, 'r')
+        path = os.path.join('194_enron', path[:-1])
+        print('# processing {} ...'.format(path))
+        if os.path.exists(path):
+            with open(path, 'r') as email:
                 email_content = parse_out_text(email)
                 final_email_content = ''
                 for word in email_content.split():
                     if word not in word_to_be_replaced:
                         final_email_content += '{} '.format(word)
                 final_email_content = final_email_content.rstrip()
-                print('======== email content after replacement')
-                print(final_email_content)
                 word_data.append(final_email_content)
-                print(name)
                 if name == 'sara':
                     from_data.append(0)
                 elif name == 'chris':
                     from_data.append(1)
-                
-                email.close()
+                    
 print('emails processed')
-print(from_data)
-print(word_data)
-
 from_sara.close()
 from_chris.close()
 
